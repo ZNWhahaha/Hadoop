9672012c86b395ab1e63f37204cbf68ffb44dcd8
ZNW_Mac <244407031@qq.com>
2020-09-07 10:38:12 +0800

    纠正连接问题

--- comhadoopFileToHbaseControl/src/main/java/com/filestohbase/hbase/FileAnlysis.java
+++ comhadoopFileToHbaseControl/src/main/java/com/filestohbase/hbase/FileAnlysis.java
@@ -10,9 +10,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -20,7 +17,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
-import java.util.Properties;
+
 
 import org.apache.hadoop.conf.Configuration;
 
@@ -52,6 +49,7 @@ public class FileAnlysis {
     private static HbaseItem FilexmlAnalysis(String filePath){
 
         HbaseItem hItem = new HbaseItem();
+        filePath = "/root/FiletoHbase/files/"+filePath;
   	    //检查出的问题。若读取xml表时，无标签，则会报错
         try {
 
@@ -110,19 +108,21 @@ public class FileAnlysis {
 
                 if(element.getElementsByTagName("abstracts").getLength() != 0){
                     hItem.abstracts = element.getElementsByTagName("abstracts").item(0).getTextContent();
-                    System.out.println("传递fundsproject成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("fundsproject").item(0).getTextContent());
+                    System.out.println("传递abstracts成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("abstracts").item(0).getTextContent());
                 }
 
 
                 if(element.getElementsByTagName("organization").getLength() != 0){
-                    System.out.println("传递fundsproject成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("fundsproject").item(0).getTextContent());
-                }
+                    System.out.println("传递organization成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("organization").item(0).getTextContent());
                     hItem.organization = element.getElementsByTagName("organization").item(0).getTextContent();
+                }
+
 
                 if(element.getElementsByTagName("paperid").getLength() != 0){
-
-                }
                     hItem.paperid = element.getElementsByTagName("paperid").item(0).getTextContent();
+                    System.out.println("传递organization成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("paperid").item(0).getTextContent());
+                }
+
 
                 System.out.println("处理多数据");
 
@@ -136,7 +136,7 @@ public class FileAnlysis {
                             hItem.autors += "," + element.getElementsByTagName("autors").item(j).getTextContent();
                         }
                     }
-                    System.out.println("传递fundsproject成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("fundsproject").item(0).getTextContent());
+                    System.out.println("autors  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("autors").item(0).getTextContent());
                 }
 
                 //String keyword
@@ -150,7 +150,7 @@ public class FileAnlysis {
                         }
 
                     }
-                    System.out.println("传递fundsproject成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("fundsproject").item(0).getTextContent());
+                    System.out.println("传递autors成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("autors").item(0).getTextContent());
                 }
 
 
@@ -165,7 +165,7 @@ public class FileAnlysis {
                             hItem.publishinghouse += ","+element.getElementsByTagName("publishinghouse").item(j).getTextContent();
                         }
                     }
-                    System.out.println("传递fundsproject成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("fundsproject").item(0).getTextContent());
+                    System.out.println("传递publishinghouse成功  "+ "("+hItem.fundsproject+")" + element.getElementsByTagName("publishinghouse").item(0).getTextContent());
                 }
 
 
@@ -265,62 +265,20 @@ public class FileAnlysis {
     //通过给定文件夹路径，查找该文件夹下的所有文件及文件名，并存储至泛型中
     //input：文件夹路线
     //output：该文件夹下所有文件的绝对路径
-    private static List<String> XMLFilePath(String localfilePath){
+    private static List<String> XMLFilePath(File[] array,int startnum){
         List<String> filepath = new ArrayList<String>();
         // get file list where the path has
-        File file = new File(localfilePath);
-        // get the folder list
-        File[] array = file.listFiles();
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = startnum; i < array.length && i < startnum+1000; i++) {
             if (array[i].isFile()) {
                 //获取文件名，文件路径，并存储到filepath中
                 // System.out.println("*****" + array[i].getPath());
                 filepath.add(array[i].getPath());
             }
-//           //用于递归调用文件夹，存储文件（当使用该部分代码时，须将泛型filepath提至全局变量，否则会出现数据覆盖的情况）
-//           else if (array[i].isDirectory()) {
-//                XMLFilePath(array[i].getPath());
-//            }
         }
         return  filepath;
     }
 
-    //发送邮件方法
-    public static void Tomail(SendEmail sendemail,String mailtext){
-        sendemail.mailFrom = "InformError@163.com";
-        sendemail.password_mailFrom="NLP111111";
-        sendemail.mailTo = "InformError@163.com";
-        sendemail.mailTittle="ERROR !!!";
-        sendemail.mailText = "test";
-        sendemail.mail_host="smtp.163.com";
-
-        try {
-            Properties prop = new Properties();
-            prop.setProperty("mail.host", sendemail.mail_host);
-            prop.setProperty("mail.transport.protocol", "smtp");
-            prop.setProperty("mail.smtp.auth", "true");
-
-            // 使用JavaMail发送邮件的5个步骤
-
-            // 1、创建session
-            Session session = Session.getInstance(prop);
-            // 开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
-            session.setDebug(true);
-            // 2、通过session得到transport对象
-            Transport ts = session.getTransport();
-            // 3、使用邮箱的用户名和密码连上邮件服务器，发送邮件时，发件人需要提交邮箱的用户名和密码给smtp服务器，用户名和密码都通过验证之后才能够正常发送邮件给收件人。
-            ts.connect(sendemail.mail_host,sendemail.mailFrom, sendemail.password_mailFrom);
-            // 4、创建邮件
-            Message message = sendemail.createSimpleMail(session,sendemail.mailFrom,sendemail.mailTo,sendemail.mailTittle,sendemail.mailText);
-            // 5、发送邮件
-            ts.sendMessage(message, message.getAllRecipients());
-            ts.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
 
 
     //控制整个程序的运行
@@ -335,21 +293,32 @@ public class FileAnlysis {
         System.out.println("Strat put files from xml to hbase");
         //初始化链接Hbase
         String TableName = args[1];
-        SendEmail sendmail = new SendEmail();
         System.out.println("初始化链接到数据库中");
         init();
-        List<String> filepath = XMLFilePath(args[0]);
-        if (filepath.size() == 0) {
-            Tomail(sendmail,"Error:find 0 file in the folder");
-        }
+        File file = new File(args[0]);
+        //File[] array = file.listFiles();
+        String[] array1 = file.list();
+        System.out.println(file.length());
+//        for (int j = 0; j < file.length();) {
+//            List<String> filepath = XMLFilePath(array,j);
+//            System.out.println("开始处理数据");
+//            System.out.println(filepath.size());
+//            //通过文件路径对每个文件进行处理
+//            for (int i = 0; i < filepath.size(); i++) {
+//                System.out.println(filepath.get(i));
+//                System.out.println("开始向HBase数据库传输数据");
+//                FileToHbase(TableName,FilexmlAnalysis(filepath.get(i)));
+//            }
+//            j = j + 1000;
+//        }
+        //List<String> filepath = array1;
         System.out.println("开始处理数据");
-        System.out.println(filepath.size());
+        //System.out.println(filepath.size());
         //通过文件路径对每个文件进行处理
-        for (int i = 0; i < filepath.size(); i++) {
-            System.out.println(filepath.get(i));
-
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println(array1[i]);
             System.out.println("开始向HBase数据库传输数据");
-            FileToHbase(TableName,FilexmlAnalysis(filepath.get(i)));
+            FileToHbase(TableName,FilexmlAnalysis(array1[i]));
         }
     }
 }
